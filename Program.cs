30e25aa591e1eb8b4d87fe2243b389981cf35788
Alkekseyt <alekseyt9@gmail.com>
2023-03-13 18:34:22 +0300

    load to git

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace RandSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var old = true;
+            var max = 0;
+            var oldMax = 0;
+            long count = 0;
+
+            while (true)
+            {
+                count++;
+                var now = rand.Next(2) == 1;
+                if (old == now)
+                {
+                    max++;
+                }
+                else
+                {
+                    if (max > oldMax)
+                    {
+                        oldMax = max;
+                        Console.WriteLine($"max: {max} count:{count}");
+                    }
+                    max = 0;
+                }
+                old = now;
+            }
+
+        }
+
+    }
+}
